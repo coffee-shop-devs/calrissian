7e0cef9ac427e1018db75dbf7b564b72d4ac52e3
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-02 04:45:39 +0000

    autocommit

--- t/server.js
+++ t/server.js
@@ -1,20 +1,29 @@
 'use strict';
 
+
+const request = require('request');
+
 test();
 
-function startServer() {
-  console.log("starting up");
-  var proc = require('child_process').spawn('node', ['../server.js'], {
+function test() {
+  // Create child process
+  console.log("Starting application");
+  var proc = require('child_process').spawn('node', ['./server.js'], {
     detached: true,
     stdio:    'inherit',
   });
+  // Check for pid from child process - if true, make a request
+  if (proc.pid) {
+    console.log(`Started application with pid ${proc.pid}`);
+    request('http://0.0.0.0:8081', function( err, res, body ){
+      if (body == "Hello World!") {
+        console.log("Test passed. Confirmed request body matched");
+      }
+    });
+  }
+  // Kill the child process
   proc.kill();
-};
-
-function test() {
-  startServer();
-  const request = require('request');
-  request('http://0.0.0.0:8081', { json: true }, (err, res, body) => {
-  if (err) console.log(err);
-  });
+  if ( proc.killed == true ) {
+    console.log('Killed application');
+  }
 }
