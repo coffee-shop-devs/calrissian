c15a6d4e0a33bbb6e868a8dd79883162aa389428
Matt Trachier <matttrach@gmail.com>
2019-12-02 07:00:16 +0000

    autocommit

--- t/server.js
+++ t/server.js
@@ -3,42 +3,17 @@
 const request = require('request');
 var proc;
 
-<<<<<<< Updated upstream
 setup();
 setTimeout(testBody,100);
 setTimeout(teardown,200);
 
 function setup() {
-=======
-//var proc;
-
-//var proc;
-
-test();
-
-function setup() {
   // Create child process
   console.log("Starting application");
   proc = require('child_process').spawn('node', ['./server.js'], {
     detached: true,
     stdio:    'inherit',
   });
-}
-
-function test() {
-
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-  // Create child process
-  console.log("Starting application");
-  proc = require('child_process').spawn('node', ['./server.js'], {
-    detached: true,
-    stdio:    'inherit',
-  });
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
   console.log(`Proc pid ${proc.pid}`);
 }
 
@@ -55,23 +30,6 @@ function testBody() {
 }
 
 function teardown() {
-=======
-=======
->>>>>>> Stashed changes
-
-  // Check for pid from child process - if true, make a request
-  if (proc.pid) {
-    console.log(`Started application with pid ${proc.pid}`);
-    request('http://0.0.0.0:8081', function( err, res, body ){
-      if (body == "Hello World!") {
-        console.log("Test passed. Confirmed request body matched");
-      } else {
-        console.log(body);
-        console.log(err);
-      }
-    });
-  }
->>>>>>> Stashed changes
   // Kill the child process
   proc.kill();
   if ( proc.killed == true ) {
