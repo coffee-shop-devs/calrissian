f5b0e39b7f1d6153d3add96cc9754e6ca6f78029
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-08 21:17:40 -0600

    Update server.js and modules

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const app     = express();
-const start   = function () {};
+const express     = require('express');
+const app         = express();
+const compression = require('compression');
+const helmet      = require('helmet');
+const fs          = require('fs');
+const css         = { style: fs.readFileSync('./views/styles.css', 'utf8')}
+const port        = 8081;
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.use([compression, helmet])
+app.listen(8081, function () {
+  console.log(`App running on port ${port}`);
+});
 
 app.get('/?', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(8081, function () {
-  console.log('Listening');
-});
-
-module.exports = start;
+  res.render('index.html', {
+    title: "Title",
+    css:   css,
+  })
+})
