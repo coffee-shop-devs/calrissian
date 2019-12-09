d1c30a5009fdad6e0fc0ab15897ea55e84b79c4d
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-08 21:47:47 -0600

    update server func and remote port const/pathway

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express     = require('express');
 const app         = express();
+const server      = app.listen(3000, function () {});
 const compression = require('compression');
 const helmet      = require('helmet');
 const fs          = require('fs');
 const css         = { style: fs.readFileSync('./views/styles.css', 'utf8')}
-const port        = 8081;
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.use([compression, helmet])
-app.listen(8081, function () {
-  console.log(`App running on port ${port}`);
-});
 
 app.get('/?', function (req, res) {
   res.render('index.html', {
