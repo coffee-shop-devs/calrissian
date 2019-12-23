0b5f1b892982780c37f4e2be8841e938b8152010
Ryan Sherer <ryan.m.sherer@gmail.com>
2019-12-23 08:48:52 -0600

    Update server port

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express     = require('express');
 const app         = express();
-const server      = app.listen(3000, function () {});
 const compression = require('compression');
 const helmet      = require('helmet');
 const fs          = require('fs');
 const css         = { style: fs.readFileSync('./views/styles.css', 'utf8')}
+
+// Change this if you have something else in mind
+var port = 3000;
+
+const server = app.listen(port, function () {});
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
